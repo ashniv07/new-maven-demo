1eb29ca8ba5302fb2bd27bc074fb373fe8d6b30a
Subashree <subashree.25cs@licet.ac.in>
2025-04-27 13:36:19 +0530

    code modified

--- src/main/java/com/example/backend/controller/HelloController.java
+++ src/main/java/com/example/backend/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from the Maven Backend!";
+        return "Hello from the Maven Backend!!!";
     }
 }
